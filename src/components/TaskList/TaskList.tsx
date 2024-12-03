0685a17b5a8a0715dadefa9921734ff1a5d1bd1f
IannisG10 <guerraiannis@gmail.com>
2024-12-03 07:41:46 +0100

    begin the TaskList component

--- src/components/TaskList/TaskList.tsx
+++ src/components/TaskList/TaskList.tsx
@@ -1,9 +1,22 @@
 import React from "react";
+import { useAppSelector } from "@/redux/hook";
 
 const TaskList: React.FC = ()=> {
+
+    const datas = useAppSelector((state) => state.data.data)
     return(
-        <>
-        </>
+        <div className="w-1/4 my-4">
+            {datas.map((item,index) => (
+                <div key={index}
+                     className="flex flex-col rounded-md shadow"
+                >
+                    {item.desc}
+                    {item.tags}
+
+                </div>
+            ))}
+
+        </div>
     )
 }
 
