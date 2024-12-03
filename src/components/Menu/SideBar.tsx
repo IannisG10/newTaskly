e59b2944f294bca46a9652d2dedba14384bcc980
IannisG10 <guerraiannis@gmail.com>
2024-12-03 09:42:45 +0100

    menu sideBar

--- src/components/Menu/SideBar.tsx
+++ src/components/Menu/SideBar.tsx
@@ -2,8 +2,12 @@ import React from "react";
 
 const SideBar: React.FC = ()=>{
     return(
-        <div className="bg-gray-200 absolute top-10 left-2 h-[370px]">
-            <h1>My side Bar</h1> 
+        <div className="flex flex-col justify-center bg-gray-200 absolute font-semibold font-josefin top-10 left-2 h-[370px] w-2/12 rounded-sm">
+            <ul className="flex flex-col justify-around h-1/2 items-center">
+                <li className="cursor-pointer">Toutes les tâches</li>
+                <li className="cursor-pointer">Tâches terminées</li>
+                <li className="cursor-pointer">Tâches supprimées</li>
+            </ul>
         </div>
     )
 }
