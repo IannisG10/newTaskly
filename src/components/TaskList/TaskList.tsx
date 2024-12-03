b20f5f9bb96623dd5c729b77cac11ba1294b7906
IannisG10 <guerraiannis@gmail.com>
2024-12-03 08:22:21 +0100

    beta design

--- src/components/TaskList/TaskList.tsx
+++ src/components/TaskList/TaskList.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { useAppSelector } from "@/redux/hook";
+import { Trash2 } from "lucide-react";
+import { Checkbox } from "../ui/checkbox";
 
 const TaskList: React.FC = ()=> {
 
@@ -8,18 +10,23 @@ const TaskList: React.FC = ()=> {
         <div className="flex flex-col gap-5 w-1/4 my-4">
             {datas.map((item,index) => (
                 <div key={index}
-                     className="flex flex-col border border-gray-100  p-4 rounded-md shadow"
+                     className="flex flex-col border gap-1 border-gray-100  p-2 rounded-md shadow"
                 >
-                    <div>
-                        {item.desc}
+                    <div className="flex justify-start items-center">
+                        <Checkbox/>
+                        <div>{item.desc}</div>
                     </div>
-                    <div>
+                    <div className="flex justify-start items-center bg-gray-400 rounded-lg p-1 text-xs w-fit font-bold">
                         {item.tags}
                     </div>
-                    <div className=" border-2 border-gray-200 rounded bg-gray-100">
-                        {item.date}
+                    <div className="flex w-1/2 justify-center items-center gap-1 ">
+                        <div className="border-2 border-gray-200  bg-gray-100 rounded p-1 text-sm">
+                            {item.date}
+                        </div>
+                        <div className="p-1 hover:bg-gray-200 rounded">
+                            <Trash2 size={20}/>
+                        </div>
                     </div>
-
                 </div>
             ))}
 
