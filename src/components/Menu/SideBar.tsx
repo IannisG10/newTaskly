9cc09ab834480f3e3342910f9f4b6803f1e8bb92
IannisG10 <guerraiannis@gmail.com>
2024-12-07 14:14:20 +0100

    Responsive sideBar

--- src/components/Menu/SideBar.tsx
+++ src/components/Menu/SideBar.tsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const SideBar: React.FC = ()=>{
     return(
-        <div className="flex flex-col justify-center bg-gray-200 absolute font-semibold font-josefin top-10 left-2 h-[370px] w-2/12 rounded-sm">
-            <ul className="flex flex-col justify-around h-1/2 items-center">
+        <div className="flex flex-col justify-center bg-gray-200 absolute font-semibold font-josefin top-10 left-2 h-[370px] md:w-2/12 w-6/12 rounded-sm">
+            <ul className="flex flex-col justify-around md:text-base text-sm h-1/2 mx-2 items-center">
                 <li className="cursor-pointer">Toutes les tâches</li>
                 <li className="cursor-pointer">Tâches terminées</li>
                 <li className="cursor-pointer">Tâches supprimées</li>
