d11a6ec1efefbfcac211944951eeaadc6c1bccf6
IannisG10 <guerraiannis@gmail.com>
2024-12-13 11:00:59 +0100

    home darkmode

--- src/components/TaskList/TaskList.tsx
+++ src/components/TaskList/TaskList.tsx
@@ -12,6 +12,8 @@ const TaskList: React.FC = ()=> {
 
     const datas = useAppSelector((state) => state.data.data);
     const dispatch = useAppDispatch();
+
+    const theme = useAppSelector((state)=> state.theme.theme)
     
    
 
@@ -63,28 +65,29 @@ const TaskList: React.FC = ()=> {
         <div className="flex flex-col gap-5 md:w-1/4 w-5/6 my-4">
             {datas.map((item) => (
                 <div key={item._id}
-                     className="flex flex-col border gap-1 relative border-gray-100  p-2 rounded-md shadow"
+                     className={`flex flex-col border gap-1 relative ${theme ? "border-gray-900 bg-slate-500":"border-gray-100"}  p-2 rounded-md shadow`}
                 >
                     <div className="flex justify-start gap-0.5 items-center font-josefin md:text-lg text-sm">
                         <Checkbox checked={item.isCheck}
                             onCheckedChange={()=>{
                                 toggleDatCheck(item._id,!item.isCheck)
                             }}
+                            
                         />
                         {
-                            item.isCheck ? <div><s>{item.desc}</s></div> : <div>{item.desc}</div>
+                            item.isCheck ? <div className={`${theme ? "text-gray-100":""}`}><s>{item.desc}</s></div> : <div  className={`${theme ? "text-gray-100":""}`}>{item.desc}</div>
                         }  
                     </div>
                     <div className="flex flex-row gap-1">
                         {item.tags.map((t,index) => (
-                            <div className="bg-gray-500 text-xs p-0.5 px-1 w-fit rounded-md font-bold font-josefin" key={index}>
+                            <div className={`${theme ? "bg-gray-700 text-gray-200 ":"bg-gray-500"} text-xs p-0.5 px-1 w-fit rounded-md font-bold font-josefin`} key={index}>
                                 {t}
                             </div>
                         ))}
                     </div>
                     <div className="flex md:w-1/2 w-3/4 justify-center md:text-base text-sm items-center gap-1 ">
-                        <div className="flex w-5/6 justify-center items-center gap-1 hover:bg-gray-300 hover:scale-95 ease-in-out transition-all duration-200 cursor-pointer
-                             border-2 border-gray-200  bg-gray-100 rounded p-1 text-sm"
+                        <div className={`flex w-5/6 justify-center items-center gap-1  hover:scale-95 ease-in-out transition-all duration-200 cursor-pointer
+                             border-2 ${theme ? "border-gray-400 bg-gray-600 hover:bg-gray-700 text-gray-200 ":"border-gray-200 bg-gray-100 hover:bg-gray-300"}   rounded p-1 text-sm`}
                              onClick={handleVueCalendar}
                              >
                             <span><Clock size={20}/></span>
@@ -96,8 +99,8 @@ const TaskList: React.FC = ()=> {
                             onClick={()=>{
                                 deleteData(item._id)
                             }}
-                            className="p-1 hover:bg-gray-200 rounded transition-all duration-200 ease-in-out 
-                                cursor-pointer">
+                            className={`p-1 ${theme ? "hover:bg-gray-600 text-gray-200":"hover:bg-gray-200"} rounded transition-all duration-200 ease-in-out 
+                            cursor-pointer`}>
 
                             <Trash2 size={20}/>
                         </div>
