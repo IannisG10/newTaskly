9cc269f6e1890186b33481ddac72ed71c46436cd
IannisG10 <guerraiannis@gmail.com>
2024-12-15 05:56:50 +0100

    REGEX form

--- src/Auth/SignUp.tsx
+++ src/Auth/SignUp.tsx
@@ -30,29 +30,35 @@ const SignUp: React.FC = ()=> {
             <div className="flex flex-col justify-start w-3/4">
                 <label htmlFor="" className="flex justify-start text-sm font-semibold">Email</label>    
                 <input type="text" 
-                        placeholder="Entrer votre email" 
+                        placeholder="Ce champ est obligatoire,Entrer votre email" 
                         className="border border-gray-200 outline-none focus:border-zinc-950 rounded-md p-1"
-                        {...register("email",{required: true})}
+                        {...register("email",{required: "L'email est necessaire",pattern: {
+                            value: /^[A-Za-z0-9._-]+@[A-Za-z0-9_-]+\.[A-Za-z{2,}$]/,
+                            message: "Veuillez entrer un email valide"
+                        }})}
                 />
-                {errors.email && <span className="text-xs text-red-500">Ce champ est obligatoire. <b>Ajouter une adresse email</b></span>}
+                {errors.email && <span className="text-xs text-red-500">{errors.email.message}</span>}
             </div>
             <div className="flex flex-col justify-start w-3/4">
                 <label htmlFor="" className="flex justify-start text-sm font-semibold">Mot de passe</label>
                 <input type="password" 
                         placeholder="Choisir un mot de passe" 
                         className="border border-gray-200 outline-none focus:border-zinc-950 rounded-md p-1"
-                        {...register("passWord",{required: true})}        
+                        {...register("passWord",{required: true,pattern: {
+                            value: /[A-Za-Z0-9]/,
+                            message: "Le moit de passe doit commencer par une majuscule"
+                        }})}        
                 />
-                {errors.passWord && <span className="text-xs text-red-500">Ce champ est obligatoire. <b>Mot de passe obligatoire</b></span>}
+               {errors.passWord && <span className="text-xs text-red-500 ">{errors.passWord.message}</span>}
             </div>
             <div className="flex flex-col justify-start w-3/4">
                 <label htmlFor="" className="flex justify-start text-sm font-semibold">Mot de passe</label>
                 <input type="password" 
                       placeholder="Confirmer votre mot de passe" 
                       className="border border-gray-200 outline-none focus:border-zinc-950 rounded-md p-1"
-                        {...register("confirmPassWord",{required: true})}  
+                        {...register("confirmPassWord",{required: "Mot de passe obligatoire"})}  
                     />
-                {errors.confirmPassWord && <span className="text-xs text-red-500">Ce champ est obligatoire. <b>Vous devez confirmer votre mot de passe</b></span>}
+                
             </div>
             <div className="w-full">
                 <button type="submit" className="bg-gray-900 text-white font-medium w-3/4 rounded-md p-1">S'inscrire</button>
