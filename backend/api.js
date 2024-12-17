7da7057c9a6fb08b0a605ab3b8c46ae000befde5
IannisG10 <guerraiannis@gmail.com>
2024-12-17 07:11:15 +0100

    install jwt

--- backend/api.js
+++ backend/api.js
@@ -2,6 +2,7 @@ const express = require('express')
 const cors = require('cors')
 const mongoose = require("mongoose")
 const bcrypt = require("bcrypt")
+const jwt = require("jsonwebtoken")
 
 const dataModel = require("./models/data")
 const users = require("./models/users")
